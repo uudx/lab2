ebf2b773951d98e3062357ea7f19007ab6289bff
uudx <155887302+uudx@users.noreply.github.com>
2025-02-01 14:12:32 +0500

    Create Operators.py

---
+++ Operators.py
@@ -0,0 +1,22 @@
+print(10 + 5)#15
+
+"""
++	Addition	x + y	
+-	Subtraction	x - y	
+*	Multiplication	x * y	
+/	Division	x / y	
+%	Modulus	x % y	
+**	Exponentiation	x ** y	
+//	Floor division	x // y
+"""
+
+print(100 + 5 * 3)#115
+
+"""
+()	Parentheses	
+**	Exponentiation	
++x  -x  ~x	Unary plus, unary minus, and bitwise NOT
+^	Bitwise XOR	
+|	Bitwise OR
+<<  >>	Bitwise left and right shifts
+"""
